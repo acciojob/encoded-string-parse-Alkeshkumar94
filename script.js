901e544e21d8faa7eef04da6edf366fc2435b27d
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-16 21:27:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 const parseCode = (str) => {
   // your code here
-	let parts=str.split("0");
-	let first=parts[0];
-	let last=parts[1];
-	let ide=parts[2];
-	return {
-		firstName:first,
-		lastName:last,
-		id:ide,
-	}
+	
+	const [firstName, lastName, id] = str.split(/0+/);
+
+    // Create and return an object with properties firstName, lastName, and id
+    return {
+        firstName: firstName,
+        lastName: lastName,
+        id: id
+    };
+	
 };
 
 // Do not change the code below

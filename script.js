9d4796f8824c78709bc1c23274681c796fdfcecf
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-16 16:02:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 const parseCode = (str) => {
   // your code here
+	let parts=str.split("0");
+	let first=parts[0];
+	let last=parts[1];
+	let ide=parts[2];
+	return {
+		firstName:first,
+		lastName:last,
+		id:ide,
+	}
 };
 
 // Do not change the code below
